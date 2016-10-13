5003f84e2cc59ac88bc3ecd7dfc34359ca92abf8
ggilestro <giorgio@gilest.ro>
2016-10-13 18:53:06 +0100

    Gave fake serial a more decent interface

--- smart_incubator/server/fake_serial.py
+++ smart_incubator/server/fake_serial.py
@@ -24,22 +24,24 @@
 
 import serial
 import time, random
+import optparse
 
 """
 This is used for debugging
 Start two serial ports with the command
+
 socat -d -d pty,raw,echo=0 pty,raw,echo=0
+
 then assign one here and one to the server
 """
 
-port = "/dev/pts/3"
-ser=serial.Serial(port, 115200)
 filename = "/home/gg/smart_incubator.csv"
 
-def random_values():
+def random_values(port, delta=5):
     """
     creates random values
     """
+    ser=serial.Serial(port, 115200)
     pause = 10
     counter = 0
     while True:
@@ -47,19 +49,19 @@ def random_values():
         counter +=1
         line = " ".join([str(i) for i in [random.randint(0,10), "R", counter, time.time(), random.uniform(18,26), random.uniform(58,67), random.randint(0, 1000), 23.0, 62.0, 100, 32400, 75600, 0]])
         ser.write(line+"\r\n")
-        time.sleep(pause)
+        time.sleep(delta)
         
         
-        
-def transfer_file(filename,verbose=False):
+def transfer_file(port, filename, downsample=5, verbose=False):
     """
     transfer CSV content to mySQL
     """
+    ser=serial.Serial(port, 115200)
+
     DD_MODE_MAP = ["DD","LD", "LL", "DL", "MM"]
-    lines_to_transfer = 1440*7*5
     
     with open(filename, "r") as fh:
-        for line in fh.readlines()[-lines_to_transfer:]:
+        for line in fh.readlines()[::downsample]:
             id, command, counter, device_time, server_time, temperature, humidity, light, set_temp, set_hum, set_light, lights_on, lights_off, dd_mode = line.strip().split(",")
             dd_mode = DD_MODE_MAP.index(dd_mode)
         
@@ -67,6 +69,22 @@ def transfer_file(filename,verbose=False):
             ser.write(lineout+"\r\n")
             if verbose: print lineout
             
-            
-#transfer_file(filename)
-random_values()
+    ser.close()
+
+
+if __name__ == '__main__':            
+
+    parser = optparse.OptionParser()
+
+    parser.add_option("-p", "--port", dest="port", default=None,help="Serial port to write to")
+    parser.add_option("-f", "--file", dest="filename", default=None, help="Transfer data from filename")
+    parser.add_option("-r", "--random", dest="random", default=False, help="Use random mode", action="store_true")
+
+
+    (options, args) = parser.parse_args()
+    option_dict = vars(options)
+
+    if option_dict['random'] and option_dict['port']: random_values(option_dict['port'])
+    if option_dict['filename'] and option_dict['port']: transfer_file( option_dict['port'], option_dict['filename'])
+    
+
